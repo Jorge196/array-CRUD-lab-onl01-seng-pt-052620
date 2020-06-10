ee4e1401a9f4f469ac34ec8eb0e8c93e02460a99
Jorge Ramirez <joramirez96@gmail.com>
2020-06-10 23:30:30 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,7 +7,7 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-  array = ["wow", "I"]
+  array = ["wow", "I", "am", ]
 end
 
 def add_element_to_start_of_array(array, element)
